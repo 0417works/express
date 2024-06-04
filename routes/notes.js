3e629aafa8e4d9fa607521d0b40dca50362805ec
0417works <0417worksmk@gmail.com>
2024-06-05 01:06:08 +0900

    Add cat api

--- routes/notes.js
+++ routes/notes.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 // 接続情報を設定
 const { MongoClient } = require("mongodb");
-const uri = "xxxx";
+const uri = "mongodb+srv://2101130067oo:ZJ7Qqdl57qmUFKvh@cluster0.sliwab4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 router.get('/', async (req, res) => {
